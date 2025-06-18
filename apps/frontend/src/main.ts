ce56172da86bd22fe9fbc527aae325c55b40fda3
gaetan <gaetan.gerard27@gmail.com>
2025-06-18 09:51:05 +0200

    update frontend to use angular 20

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,7 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
-}).catch((err) => console.error(err));
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
